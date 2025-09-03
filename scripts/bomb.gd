1466f7ea7889a80af1e511074c5a272b900044c1
Tanish Somashekhar Desai <tanshikode79@gmail.com>
2025-09-03 17:03:54 +0530

    Added bomb-follow physics

--- scripts/bomb.gd
+++ scripts/bomb.gd
@@ -1,11 +1,14 @@
-extends StaticBody2D
+extends RigidBody2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	gravity_scale = 0
 	position = get_viewport_rect().size / 2
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Explosion.play()
+	linear_velocity = get_viewport().get_mouse_position() - position
+	pass
