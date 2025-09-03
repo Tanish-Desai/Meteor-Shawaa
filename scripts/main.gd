8b8e1bf95a4126305e7baaadf983f9f5bff2aa58
Tanish Somashekhar Desai <tanshikode79@gmail.com>
2025-09-03 21:32:24 +0530

    Cleaned the code, changed var names
    - launchX, launchY and launch_padding are now spawnX, spawnY and spawn_pad

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,21 +1,25 @@
 extends Node2D
 
 @export var bomb_scene : PackedScene
-var launch_padding : float = 10
+var spawn_pad : float = 10
+var viewport_size : Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	viewport_size = get_viewport_rect().size
 	var bomb = bomb_scene.instantiate()
 	var side : int = randi_range(1,4)
-	var launchX : float
-	var launchY : float
-	if side==1 or side==3:
-		launchX = randf_range(0,get_viewport_rect().size.x)
-		launchY = -launch_padding if side==1 else (get_viewport_rect().size.y + launch_padding) 
+	var spawnX : float
+	var spawnY : float
+	if side%2!=0:
+		# top and bottom
+		spawnX = randf_range(0,viewport_size.x)
+		spawnY = -spawn_pad if side==1 else (viewport_size.y + spawn_pad) 
 	else:
-		launchY = randf_range(0, get_viewport_rect().size.y)
-		launchX = -launch_padding if side==4 else (get_viewport_rect().size.y + launch_padding) 
-	bomb.position = Vector2(launchX, launchY)
+		# right and left
+		spawnY = randf_range(0, viewport_size.y)
+		spawnX = -spawn_pad if side==4 else (viewport_size.x + spawn_pad) 
+	bomb.position = Vector2(spawnX, spawnY)
 	
 	add_child(bomb) # this adds bomb to the scene_tree, else it will be killed after _ready
 	pass # Replace with function body.
